5cf31b2fbd7e929c2230894b14bbfd60ca37efd1
sung <shan20030723@gmail.com>
2022-04-13 05:48:03 +0800

    hi

---
+++ 點唱機/playList.js
@@ -0,0 +1,12 @@
+//YouTube Video ID
+let playList = [
+    "5E6SfhHPHeQ",
+    "9atLLuPKBZ8",
+    "kHZtFzm8AWA"
+    ];
+    //播放起訖秒數
+    let playTime = [
+    [65,76],
+    [96,110],
+    [52,76]
+    ];
